2d5615b073891ba7444e06b91368d78a69310c5b
Nicolò Buganza <nicolo.buganza@tiscali.it>
2024-12-18 13:29:11 +0100

    Create primoScript.sh

---
+++ 3BI/primoScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello World!"
+
+#variabili, non hanno tipo (tutto è una stringa)
+x=4
+
+echo "la variabile x vale: $x"
+
+read -p "Dammi il tuo nome" nome
+echo "Il mio nome è $nome"
