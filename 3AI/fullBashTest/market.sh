e1d2f1be74f8ae9ab991e24ad8f7c057663fb931
iTzNikk <nicolo.buganza@tiscali.it>
2025-05-18 16:57:11 +0200

    Update market.sh

--- 3AI/fullBashTest/market.sh
+++ 3AI/fullBashTest/market.sh
@@ -14,6 +14,7 @@ pioggiaSubita=0
 
 init() {
 	read -p "Il tuo nome? " name
+	#Un controllo in più, come mai è qui?
 	if [ -z $name ]
 	then
 		name="MercanteGranpasso"
@@ -30,44 +31,65 @@ simulateWeek(){
 		action=$((RANDOM % 5))
 		case $action in
 			0)
-				eventi[${#eventi[@]}]="Vendi spezie:$capitale"
 				((capitale+=15))
 				((spezieVendute++))
 				echo "Oggi ha venduto delle spezie. Il suo capitale aumenta di 15 monete ($capitale)"
+				eventi[${#eventi[@]}]="Vendi spezie:$capitale"
+
 			;;
 			1)
-				eventi[${#eventi[@]}]="Acquisti cavallo:$capitale"
 				((capitale-=40))
 				((cavalliAcquistati++))
 				echo "Oggi ha acquistato un cavallo. Il suo capitale diminuisce di 40 monete ($capitale)"
+				eventi[${#eventi[@]}]="Acquisti cavallo:$capitale"
+
 			;;
 			2)
-				eventi[${#eventi[@]}]="Vendi vino:$capitale"
 				((capitale+=20))
 				((vinoVenduto++))
 				echo "Oggi ha venduto del vino. Il suo capitale aumenta di 20 monete ($capitale)"
+				eventi[${#eventi[@]}]="Vendi vino:$capitale"
+
 			;;
 			3)
-				eventi[${#eventi[@]}]="Tassazione regale:$capitale"
 				((capitale-=10))
 				((tassePagate++))
 				echo "Oggi è stato tassato dai reali. Il suo capitale diminuisce di 10 monete ($capitale)"
+				eventi[${#eventi[@]}]="Tassazione regale:$capitale"
+
 			;;
 			4)
-				eventi[${#eventi[@]}]="Pioggia rovina bancarella:$capitale"
 				((capitale-=25))
 				((pioggiaSubita++))
 				echo "Oggi la pioggia ha danneggiato la sua bancarella. Il suo capitale diminuisce di 25 monete ($capitale)"
+				eventi[${#eventi[@]}]="Pioggia rovina bancarella:$capitale"
+
 			;;
 		esac
 		if [ $capitale -le 0 ]
 		then
 			echo "$name, sfortunatamente il suo patrimonio si è esaurito."
 			showReport
+			saveReportToFile
 			exit 0
 		fi
 	done
+	bonuses=(10 20 50)
+	weekBonusIndex=$((RANDOM % ${#bonuses[@]}))
+	weekBonus=${bonuses[$weekBonusIndex]}
+	echo "Hai terminato con successo una settimana di lavoro, $name. I reali ti donano $weekBonus monete"
+	((capitale+=$weekBonus))
+}
 
+saveReportToFile(){
+	location=$HOME/Desktop
+	cd $location
+
+	#per semplicità...
+	mkdir -p reportMercato
+	cd reportMercato
+	file="$location/reportMercato/resoconto_$name.txt"
+	echo $(showReport) > $file
 }
 
 showReport() {
@@ -84,9 +106,9 @@ showReport() {
 	echo "Pioggia subita: $pioggiaSubita"
 	echo "Tasse pagate: $vinoVenduto"
 	echo "Spezie vendute: $spezieVendute"
-
 }
 
 init
 simulateWeek
 showReport
+saveReportToFile
